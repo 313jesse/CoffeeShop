a8933f7bea7bb832b696cbc0ef49f79ebe57b074
unknown <jess7642@gmail.com>
2018-08-16 10:48:06 -0400

    Coffee Shop

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        public ActionResult AddUser(UserInfo newUser)
+        {
+
+            //add validation!
+
+            if (ModelState.IsValid)
+            {
+                //send the data to DB
+
+
+
+                //confirmation or send to index page etc. 
+
+
+
+                //     ViewData["ConfMessage"] = "Thanks" + newUser.FirstName; //regular dictionary (older style) does the same thing as next line)
+
+                ViewBag.ConfMessage = "Thanks for signing up " + newUser.FirstName;   //dynamic dictionary   .ConfMessage is a new variable here. 
+                                                                       // viewbag data only goes one way from controller to view only.     controller -----> view     
+                                                                       //viewbag is a dynamic object. 
+
+
+
+                //view should have a different name, because it only shows the data. 
+
+                return View("Confirm");
+            }
+
+
+            else
+            {
+                return View("Error");
+            }
+            //changed name to confirm to be more specific for other developers.
+        }
+    }
+}
---
+++ WebApplication3/Models/UserInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public class UserInfo
+    {
+        [Required]   //THis is called an "Attribute" adding this here will make the field required. 
+        [RegularExpression("^[a-zA-Z]{3,30}$")]
+        public string FirstName { set; get; }
+        [Required]
+        [RegularExpression("^[a-zA-Z]{2,25}$")]
+        public string LastName { set; get; }
+
+        [Required]
+        [RegularExpression("^[0-9]{3}-[0-9]{3}-[0-9]{4}$")]
+        public string Phone { set; get; }
+           
+
+        public UserInfo()
+        {
+            FirstName = ""; 
+            LastName = "";
+            Phone = "";
+        }
+
+        public UserInfo(string fn, string ln, string ph)
+            {
+                FirstName = fn;
+                LastName = ln;
+            Phone = ph;
+            }
+        }
+    }
